0ca2043dfa8ad34b676d3741efbeef6d816e822c
Altair <Altair@Altair.local>
2016-02-24 18:14:44 +0100

    Initial commit

---
+++ app/src/main/java/com/twitter/MainActivity.java
@@ -0,0 +1,65 @@
+package com.twitter;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends ActionBarActivity {
+    private ImageView img;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img=(ImageView)findViewById(R.id.imageView);
+        Button b1=(Button)findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+//                Uri screenshotUri = Uri.parse("android.resource://com.twitter/*");
+                String tweetUrl = "https://twitter.com/intent/tweet?text=prueba HERE &url="
+                        + "https://www.google.com";
+                Uri uri = Uri.parse(tweetUrl);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+//                try {
+//                    InputStream stream = getContentResolver().openInputStream(screenshotUri);
+//                } catch (FileNotFoundException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//                sharingIntent.setType("image/jpeg");
+//                sharingIntent.putExtra(Intent.EXTRA_STREAM, screenshotUri);
+//                startActivity(Intent.createChooser(sharingIntent, "Share image using"));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
